04d4d37491391b7f565df2a22b73d8a75d3d5d0c
AAspCodes <asp.anthony1@gmail.com>
2021-04-30 14:27:38 -0700

    clang format

--- main.cpp
+++ main.cpp
@@ -25,7 +25,8 @@ int main()
     gen();
     printecosystem();
 
-    for (int i = 0; i < 100; i++){
+    for (int i = 0; i < 100; i++)
+    {
         // count neighbors
         countneighbors();
         // check if above number kill, if below birth.
@@ -34,47 +35,58 @@ int main()
         printecosystem();
         // wait
         nanosleep(&tim, &tim2);
-    } 
-
+    }
     return 0;
 }
 
-void gen(){
-    v.resize(HEIGHT, vector<int>(WIDTH,0));
-
-    for (int i = 0; i < HEIGHT; i++){
-        for (int j = 0; j < WIDTH; j++){
+void gen()
+{
+    v.resize(HEIGHT, vector<int>(WIDTH, 0));
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        for (int j = 0; j < WIDTH; j++)
+        {
             v[i][j] = !(rand() % 6);
         }
     }
 }
 
-void printout(vector<vector<int> > &vec){
-    for (int i = 0; i < HEIGHT; i++){
-        for (int j = 0; j < WIDTH; j++){
+void printout(vector<vector<int> > &vec)
+{
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        for (int j = 0; j < WIDTH; j++)
+        {
             cout << vec[i][j];
         }
         cout << endl;
     }
 }
 
-void countneighbors(){
-    n.resize(HEIGHT, vector<int>(WIDTH,0));
+void countneighbors()
+{
+    n.resize(HEIGHT, vector<int>(WIDTH, 0));
 
-    for (int i = 0; i < HEIGHT; i++){
-        for (int j = 0; j < WIDTH; j++){
-            int imin = i == 0 ? 0: i - 1;
-            int imax = i + 2 >= HEIGHT ? HEIGHT: i + 2;
-            int jmin = j == 0 ? 0: j - 1;
-            int jmax = j + 2 >= WIDTH ? WIDTH: j + 2;
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        for (int j = 0; j < WIDTH; j++)
+        {
+            int imin = i == 0 ? 0 : i - 1;
+            int imax = i + 2 >= HEIGHT ? HEIGHT : i + 2;
+            int jmin = j == 0 ? 0 : j - 1;
+            int jmax = j + 2 >= WIDTH ? WIDTH : j + 2;
             int sumneighbors = 0;
 
-            for (int y = imin; y < imax; y++){
-                for (int x = jmin; x < jmax; x++){
-                    if ((i==y) && (j==x)){
+            for (int y = imin; y < imax; y++)
+            {
+                for (int x = jmin; x < jmax; x++)
+                {
+                    if ((i == y) && (j == x))
+                    {
                         continue;
                     }
-                    if (v[y][x]){
+                    if (v[y][x])
+                    {
                         sumneighbors++;
                     }
                 }
@@ -85,27 +97,38 @@ void countneighbors(){
     }
 }
 
-void playgod(){
-    for (int i = 0; i < HEIGHT; i++){
-        for (int j = 0; j < WIDTH; j++){
+void playgod()
+{
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        for (int j = 0; j < WIDTH; j++)
+        {
             int nval = n[i][j];
-            
-            if (v[i][j]){
-                if (nval != 3 && nval != 2){
+
+            if (v[i][j])
+            {
+                if (nval != 3 && nval != 2)
+                {
                     v[i][j] = 0;
                 }
-            } else if (nval == 3) {
+            }
+            else if (nval == 3)
+            {
                 v[i][j] = 1;
             }
         }
     }
 }
 
-void printecosystem(){
-    for (int i = 0; i < HEIGHT; i++){
-        for (int j = 0; j < WIDTH; j++){
-            cout << (v[i][j] ? "#": "  ");
+void printecosystem()
+{
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        for (int j = 0; j < WIDTH; j++)
+        {
+            cout << (v[i][j] ? "#" : "  ");
         }
+
         cout << endl;
     }
     cout << "\n\n\n\n\n";
